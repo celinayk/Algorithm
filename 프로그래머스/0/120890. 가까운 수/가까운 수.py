f22780d99c81762d1a66207b9767fb7482ed1e4f
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2024-05-07 20:24:15 +0900

    [level 0] Title: 가까운 수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,10 +1,8 @@
 def solution(array, n):
+    array.sort()
     ans =[]
     for i in array:
         num=abs(i-n)
         ans.append(num)
-    min_diff=min(ans)
-    closest_nums = [array[i] for i in range(len(array)) if ans[i] == min_diff] 
-    return min(closest_nums)  
-    #return array[ans.index(min(ans))]
+    return array[ans.index(min(ans))]
     
