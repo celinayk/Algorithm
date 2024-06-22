f49b7cd66682cb6c94f4c32a0c03af3c0b2bafd4
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2024-06-22 21:33:36 +0900

    [level 0] Title: OX퀴즈, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,2 +1,15 @@
 def solution(quiz):
-    return ["O" if eval(q.replace("=","==")) else "X" for q in quiz]
+    ans = []
+    for q in quiz:
+        a,b = q.split("=")
+        l,op,r = a.split()
+        if op=='+':
+            result = int(l) + int(r)
+        else:
+            result = int(l) - int(r)
+        if result == int(b):
+            ans.append("O")
+        else:
+            ans.append("X")
+    return ans
+    
