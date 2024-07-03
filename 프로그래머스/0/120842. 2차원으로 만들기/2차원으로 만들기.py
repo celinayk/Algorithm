39ba4e4ff55be1d2c65368638f849bb72f0b6267
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2024-07-03 21:16:29 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,5 +1,2 @@
 def solution(num_list, n):
-    ans=[]
-    for i in range(0, len(num_list), n):
-        ans.append(num_list[i:i+n])
-    return ans
+    return[num_list[i:i+n] for i in range(0, len(num_list), n)]
