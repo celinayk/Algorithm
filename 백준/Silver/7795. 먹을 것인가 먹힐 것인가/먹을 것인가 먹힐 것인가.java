49257e1512b629abee584a2f2e6261ade419b5a8
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2024-08-19 21:03:48 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 400 ms, Memory: 37520 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+        
+        int t = Integer.parseInt(br.readLine());	
+        for(int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int n = Integer.parseInt(st.nextToken());  
+            int m = Integer.parseInt(st.nextToken());  
+            
+            int arr[] = new int[n];
+            int brr[] = new int[m];
+      
+            
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j=0; j<arr.length; j++) {
+            	arr[j] = Integer.parseInt(st.nextToken());
+            }
+            
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j=0; j<brr.length; j++) {
+            	brr[j] = Integer.parseInt(st.nextToken());
+            }
+            
+            Arrays.sort(arr);
+            Arrays.sort(brr);
+            
+            
+            int ans=0;
+            for(int j=0; j<brr.length; j++) {
+            	ans+=((arr.length)-upperbound(arr,brr[j]));
+            	
+            }
+            System.out.println(ans);
+          
+        }
+    }
+
+	private static int upperbound(int[] arr, int brr) {
+		int low = 0;
+		int high = arr.length;
+		
+		while(low<high) {
+			int mid = (low+high)/2;
+			if(arr[mid]>brr) {
+				high = mid;
+			}
+			else {
+				low = mid+1;
+			}
+		}
+		return low;
+	}
+}
+
+
+
+
+
+
