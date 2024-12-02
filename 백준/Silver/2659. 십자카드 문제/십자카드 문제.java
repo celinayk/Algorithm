042d332455deb0aba93444ebf52bb80b51e8684d
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2024-12-02 17:25:32 +0900

    [Silver III] Title: 십자카드 문제, Time: 144 ms, Memory: 19164 KB -BaekjoonHub

--- 백준/Silver/2659. 십자카드 문제/십자카드 문제.java
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.java
@@ -1,77 +1,60 @@
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
-	static int clockNum;
-	static int ans = 0;
-    public static void main(String[] args) throws IOException {
+	static int small;
+	static int cnt=0;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int []arr = new int[4];
+
+        int [] num = new int[4];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<4; i++) {
-        	arr[i] = Integer.parseInt(st.nextToken());
+        	num[i] = Integer.parseInt(st.nextToken());
         }
         
-         clockNum = getClockNum(arr);
-         findSmallClockNum();
-         System.out.println(ans);
+        small=searchNum(num);
+        findSmallClockNum();
+        System.out.println(cnt);
         
-    }	
-    
-    //시계수 구하기 
-    static int getClockNum(int[] arr) {
-    	 int num = 10000;      
-         StringBuilder sb;
-         for(int i=0; i<4; i++) {
-         	sb = new StringBuilder();
-         	for(int j=0; j<4; j++) {
-         		sb.append(arr[(i + j) % 4]);    		
-         	}
-         	int cur = Integer.parseInt(sb.toString());
-     		num = Math.min(cur, num);
-         } 
-         return num;
     }
-    
-    
-    //시계수보다 작은 
-    static void findSmallClockNum() {
-    	//전체 4자리 숫자들중에서 몇번째에 해당하는지 찾기
-        for(int i=1111; i<=clockNum; i++) {
-        	if(String.valueOf(i).contains("0")) continue;
-        	
-        	//4자리수 1111부터 9999까지 
-        	int[] digit = new int[4];
-        	int k = i;
-        	for(int j=3; j>=0; j--) {
-        		digit[j] = k%10;
-        		k/=10;
-        	}
-        	 if (getClockNum(digit) == i) ans++; 
-        	
 
-        }
-    	
-    }
+    // 몇번째로 작은 시계수인지 찾는 메서드
+    private static void findSmallClockNum() {
+		for(int i=1111; i<=small; i++) {
+			if(String.valueOf(i).contains("0")) continue;
+			// 숫자를 각 자리로 분리
+			int [] digits = new int[4];
+			int tmp = i;
+			for(int j=3; j>=0; j--) {
+				digits[j]= tmp%10;
+				tmp/=10;
+			}
+			
+			int a = searchNum(digits);
+			if(a==i) cnt++;
+		
+		}
+		
+	}
+
+	// 시계수 찾는 메서드 
+	private static int searchNum(int[] arr) {
+		int tmp = Integer.MAX_VALUE;
+		
+		for(int i=0; i<4; i++) {
+			StringBuilder sb = new StringBuilder();
+			for(int j=0; j<4; j++) {
+				sb.append(arr[(i + j) % 4]);	  
+			}
+			tmp = Math.min(tmp, Integer.parseInt(sb.toString()));
+		}
+		return tmp;
+	}
 }
-	
-
 
