beccda52b758350a28ceb2d0adafa8e30fb2e644
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2025-08-06 14:46:54 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,51 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
+
 
 public class Main {
-	   public static void main(String[] args) throws IOException {
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        int n = Integer.parseInt(br.readLine());
-	        
-	        int [] arr = new int[n];
-	        int [] sum = new int[n];
-	        
-	     
-	        StringTokenizer st = new StringTokenizer(br.readLine());
-	        for(int i=0; i<n; i++) {
-	        	arr[i] = Integer.parseInt(st.nextToken());
-	        }
-	     
-	        
-	        Arrays.sort(arr);
-	        
-	        for(int i=0; i<n; i++) {
-	        	sum[i] = arr[i];
-	        	for(int j=0; j<i; j++) {
-	        		sum[i]+=arr[j];
-	        	}
-	        }
-	        
-	        int ans = 0;
-	        for(int l: sum) {
-	        	ans+=l;
-	        }
-	        System.out.println(ans);
-	   }
+    public static void main(String[] args) throws Exception, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] person =  new int[n];
+
+        for (int i = 0; i < n; i++) {
+            person[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(person);
+
+        int[] ans = new int[n];
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            sum+=person[i];
+            ans[i] = sum;
+        }
+
+        int cnt = 0;
+        for(int k: ans) {
+            cnt+=k;
+        }
+
+        System.out.println(cnt);
+    }
 }
