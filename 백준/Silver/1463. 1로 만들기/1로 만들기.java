ff7ad25ff2c7b58adff6fdefc07aa77b777684c2
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2025-08-23 17:16:12 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18092 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,52 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
-	static Integer dp[];
-	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-             
-        int n = Integer.parseInt(br.readLine());
-        
-        dp= new Integer[n+1];
-        
-        dp[0] = dp[1] = 0;
-        
-        System.out.println(recur(n));
-        
+    public static void main(String[] args) throws Exception, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n =  Integer.parseInt(br.readLine());
+        int dp[] = new int[n+1];
+        dp[0]=dp[1]=0;
+
+        for(int i=2; i<=n; i++) {
+            //1뺄떄
+            dp[i] = dp[i-1] +1;
+            //2나누기
+            if(i%2==0) {
+                dp[i] = Math.min(dp[i], dp[i/2]+1);
+            }
+            //3나누기
+            if(i%3==0) {
+                dp[i] = Math.min(dp[i], dp[i/3]+1);
+            }
+
+        }
+        System.out.println(dp[n]);
     }
-
-	static int recur(int n) {
-		
-		if(dp[n] == null) {
-			if(n%6==0) {
-				dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
-			}
-			
-			else if(n%3==0) {
-				dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
-			}
-			else if(n%2==0) {
-				dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
-			}
-			else {
-				dp[n] = recur(n - 1) + 1;
-			}
-		}
-		return dp[n];
-	}
-
 }
-
