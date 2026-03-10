c6116b893052d3c2c4fc9c5743fd2965d217e278
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2026-03-10 23:59:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 640 ms, Memory: 150240 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,65 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
-	static LinkedList<Integer>[] adjList;
-	static boolean[]isVisited;
-	 static int cnt = 0;
-	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-             
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        
-        isVisited = new boolean[n+1];
-        adjList = new LinkedList[n+1]; 
-        
+    static int n,m;
+    static int cnt = 0;
+    static boolean[] visited;
+    static LinkedList<Integer>[] adj;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =  new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        // 초기화
+        visited = new boolean[n+1];
+        adj  = new LinkedList[n+1];
+
+        // 인접 리스트 초기화
         for(int i=0; i<=n; i++) {
-        	adjList[i] = new LinkedList<Integer>();
+            adj[i] = new LinkedList<Integer>();
         }
-        
-        //그래프 구현
+
         for(int i=0; i<m; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int u = Integer.parseInt(st.nextToken());
-        	int v = Integer.parseInt(st.nextToken());
-        	
-        	adjList[u].add(v);
-        	adjList[v].add(u);
+            st =  new StringTokenizer(br.readLine());
+            int u =  Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            adj[u].add(v);
+            adj[v].add(u);
         }
-        
-       
+
         for(int i=1; i<=n; i++) {
-        	if(!isVisited[i]) {
-        		dfs(i);
-        		cnt++;
-        	}
+            if(!visited[i]) {
+                dfs(i);
+                cnt++;
+            }
         }
         System.out.println(cnt);
-        
+
+
     }
 
-	private static void dfs(int i) {
-		isVisited[i] = true;
-		
-		for(int j :adjList[i]) {
-			if(!isVisited[j]) {
-				dfs(j);
-			}
-			
-		}
-	}
+    private static void dfs(int k) {
+        visited[k] = true;
+
+        for (int i : adj[k]) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
 }
+
