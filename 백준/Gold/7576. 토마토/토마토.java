8525810962a8eb915f5225ce76813ec53061b757
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2026-03-10 23:02:54 +0900

    [Gold V] Title: 토마토, Time: 608 ms, Memory: 120832 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,33 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import java.util.Collections;
+
 
 public class Main {
-    static int n,m,day;
-    static int[][] arr;
+    static int n,m;
     static boolean[][] visited;
+    static int[][] adj;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    static Queue<int[]> q = new LinkedList<>();
-
+    static int ans = 0;
+    static Queue<int[]>q;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
 
-        arr = new int[n][m];
+        // 배열 초기화
         visited = new boolean[n][m];
-        for(int i=0; i<n; i++) {
+        adj = new int[n][m];
+        q = new LinkedList<>();
+
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++) {
-                arr[i][j] =  Integer.parseInt(st.nextToken());
-                if(arr[i][j] == 1) {
-                    q.add(new int[]{i,j});
+            for(int j = 0; j < m; j++) {
+                adj[i][j] = Integer.parseInt(st.nextToken());
+                if(adj[i][j] == 1) {
+                    q.offer(new int[]{i, j});
                 }
             }
         }
@@ -35,29 +35,23 @@ public class Main {
         System.out.println(bfs());
 
 
-        // 처음부터 토마토가 전부 익었으면 0
-
-        // 토마토가 모두 익지 못하는 상황이면 -1
-
-
-
     }
 
-    private static int bfs() {
+    public static int bfs() {
         while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
+            int[] tmp = q.poll();
+            int cx = tmp[0];
+            int cy = tmp[1];
 
             for(int i=0; i<4; i++) {
-                int nx = cx+dx[i];
-                int ny = cy+dy[i];
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
-                if(nx>=0 && nx<n && ny>=0 && ny<m ) {
-                    if(arr[nx][ny]==0 && !visited[nx][ny]) {
+                if(nx>=0 && ny>=0 && nx<n && ny<m) {
+                    if(!visited[nx][ny] && adj[nx][ny]==0) {
+                        q.offer(new int[]{nx,ny});
                         visited[nx][ny] = true;
-                        q.add(new int[]{nx,ny});
-                        arr[nx][ny] = arr[cx][cy]+1; // 날짜 더하기
+                        adj[nx][ny] = adj[cx][cy]+1;
                     }
                 }
             }
@@ -65,20 +59,22 @@ public class Main {
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(arr[i][j]==0) {
+                if(adj[i][j]==0) { // 익지 않은 토마토가 남아 있는 경우
                     return -1;
                 }
-                day = Math.max(day, arr[i][j]);
+                ans = Math.max(ans, adj[i][j]);
             }
         }
 
-        if(day==1) {
+        if(ans==1) {
             return 0;
         } else {
-            return day-1;
+            return ans-1;
         }
 
+
     }
+
 
 }
 
