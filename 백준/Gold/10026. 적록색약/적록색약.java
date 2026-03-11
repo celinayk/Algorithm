9a05f38b0135ff92e6db2e355534bd754a8c078d
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2026-03-11 22:46:16 +0900

    [Gold V] Title: 적록색약, Time: 124 ms, Memory: 16236 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,99 +1,78 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
+
 
 public class Main {
-	static char[][] arr;
-	static boolean[][] visited;
-	static int dx[] = {-1,1,0,0};
-	static int dy[] = {0,0,-1,1};
-	static int n;
-    public static void main(String[] args) throws IOException {
+    static int n;
+    static boolean[][] visited;
+    static int[][] adj;
+    static int cnt = 0;
+    static int ans = 0;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         n = Integer.parseInt(br.readLine());
-        arr = new char[n][n];
+
         visited = new boolean[n][n];
-        
- 
-        for(int i=0; i<n; i++) {
-        	String line = br.readLine();
-        	for(int j=0; j<n; j++) {
-        		arr[i][j] = line.charAt(j);
-        	}
+        adj = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for(int j = 0; j < n; j++) {
+                adj[i][j] = line.charAt(j);
+            }
         }
-        
-        StringBuilder sb = new StringBuilder();
-        int cnt=0;
-        for(int i=0; i<n; i++) {
-        	for(int j=0; j<n; j++) {
-        		if(!visited[i][j]) {
-        			dfs(i,j);
-        			cnt++;
-        		}
-        	}
+
+        // 정상인
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!visited[i][j]) {
+                    dfs(i,j);
+                    cnt++;
+                }
+            }
         }
-        sb.append(cnt);
-        sb.append(" ");
-        
+        System.out.print(cnt+ " ");
+
+        // 적록색약
         visited = new boolean[n][n];
-        for(int i=0; i<n; i++) {
-        	for(int j=0; j<n; j++) {
-        		if(arr[i][j]=='G') {
-        			arr[i][j] = 'R';
-        		}
-        	}
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(adj[i][j] == 'G') {
+                    adj[i][j] = 'R';
+                }
+            }
         }
-        
-        int cnt2=0;
-        for(int i=0; i<n; i++) {
-        	for(int j=0; j<n; j++) {
-        		if(!visited[i][j]) {
-        			dfs(i,j);
-        			cnt2++;
-        		}
-        	}
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!visited[i][j]) {
+                    dfs(i,j);
+                    ans++;
+                }
+            }
         }
-        sb.append(cnt2);
-        System.out.println(sb);
-        
-        
+        System.out.println(ans);
+
     }
 
-	private static void dfs(int x, int y) {
-		visited[x][y] = true;
-		char now = arr[x][y];
-		
-		for(int i=0; i<4; i++) {
-			int nx = dx[i] + x;
-			int ny = dy[i] + y;
-			
-			if(nx<n && ny<n && nx>=0 && ny>=0) {
-				if(!visited[nx][ny]) {
-					if(arr[nx][ny]==now) {
-						dfs(nx,ny);
-					}
-				}
-			}
-		}
-		
-	}
-	
+    static void dfs(int x, int y) {
+        visited[x][y] = true; // r,r
+
+        for(int i=0; i<4; i++) {
+            int nx =  x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny]) {
+                if(adj[nx][ny] == adj[x][y]) { // 같은 색상이면 r=r / g=g / b=b
+                    dfs(nx,ny);
+                }
+            }
+        }
+    }
+
+
 }
 
