d60fc03bb61a93cb09aea9185930ca5db71503cc
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2026-03-12 21:50:52 +0900

    [Silver I] Title: 나이트의 이동, Time: 332 ms, Memory: 114808 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,70 +1,66 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+
 
 public class Main {
-	static int[][] arr;
-	static boolean[][] visited;
-	static int l;
-	static int dx[] = {-2,-1,1,2,2,1,-1,-2};
-	static int dy[] = {-1,-2,-2,-1,1,2,2,1};
-	static int start_x, start_y, finish_x, finish_y;
+    static int n, t;
+    static boolean[][] visited;
+    static int[][] adj;
+    static int start_x, start_y, finish_x, finish_y;
+    static int[] dx = {-2,-1,1,2,2,1,-1,-2};
+    static int[] dy = {-1,-2,-2,-1,1,2,2,1};
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        
-        for(int t=0; t<T; t++) {
-        	l = Integer.parseInt(br.readLine());
-        	arr=new int[l][l];
-        	visited = new boolean[l][l];
-        	
-        	
-        	st = new StringTokenizer(br.readLine());
-        	start_x = Integer.parseInt(st.nextToken());
-        	start_y = Integer.parseInt(st.nextToken());
-        	
-        	st = new StringTokenizer(br.readLine());
-        	finish_x = Integer.parseInt(st.nextToken());
-        	finish_y = Integer.parseInt(st.nextToken());
-        	
-        	bfs();
-        	sb.append(arr[finish_x][finish_y]).append("\n");
-        	
-        }
-        System.out.println(sb);
-    }
-	private static void bfs() {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {start_x,start_y});
-		visited[start_x][start_y] = true;
-		
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringBuilder sb = new StringBuilder();
+       t = Integer.parseInt(br.readLine());
+       for(int k = 0; k < t; k++) {
+           // 체스판 크기
+           n = Integer.parseInt(br.readLine());
 
-		while(!q.isEmpty()) {
-			int[] now= q.poll();
-			int cx = now[0];
-			int cy = now[1];
-			
-			for(int i=0; i<8; i++) {
-				int nx = dx[i]+cx;
-				int ny = dy[i]+cy;
-				
-				if(nx>=0 && ny>=0 && nx<l && ny<l) {
-					if(!visited[nx][ny]) {
-						q.add(new int[] {nx,ny});
-						visited[nx][ny] = true;
-						arr[nx][ny] = arr[cx][cy]+1;
-					}
-				}
-			}
-		}
-		
-		
-	}
-	
+           // 초기화
+           visited = new boolean[n][n];
+           adj = new int[n][n];
+
+           StringTokenizer st = new StringTokenizer(br.readLine());
+           start_x = Integer.parseInt(st.nextToken());
+           start_y = Integer.parseInt(st.nextToken());
+
+           st = new StringTokenizer(br.readLine());
+           finish_x = Integer.parseInt(st.nextToken());
+           finish_y = Integer.parseInt(st.nextToken());
+
+           bfs();
+           System.out.println(adj[finish_x][finish_y]);
+
+
+       }
+    }
+
+    static void bfs() {
+        Queue<int[]>q = new LinkedList<>();
+        q.offer(new int[]{start_x, start_y});
+        visited[start_x][start_y] = true;
+
+        while(!q.isEmpty()) {
+            int[] tmp = q.poll();
+            int cx = tmp[0];
+            int cy = tmp[1];
+
+            for(int i=0; i<8; i++) {
+                int nx = cx+ dx[i];
+                int ny = cy+ dy[i];
+
+                if(nx >=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny]) {
+                    q.add(new int[]{nx,ny});
+                    visited[nx][ny] = true;
+                    adj[nx][ny] = adj[cx][cy] + 1;
+                }
+
+            }
+        }
+    }
+
+
 }
 
