d68be84532cebf0055d1cdfafc3e82004646c526
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2026-03-17 23:45:36 +0900

    [Silver II] Title: 섬의 개수, Time: 152 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,83 +1,68 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
+    static int w, h;
+    static boolean[][] visited;
+    static int[][] arr;
+    static int[] dx = {-1,1,0,0,-1,-1,1,1};
+    static int[] dy = {0,0,-1,1,-1,1,-1,1};
+    static int cnt = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	static boolean[][] isVisited;
-	static int[][] arr;
-	static int w,h;
-	static int[] dx = {-1,1,0,0,-1,-1,1,1};
-	static int[] dy = {0,0,-1,1,-1,1,-1,1};
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            if(w== 0 && h==0) {
+                break;
+            }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-        StringBuilder sb = new StringBuilder();
-        
-        while(true) {
-        	String[] input = br.readLine().split(" ");
-            w = Integer.parseInt(input[0]);
-            h = Integer.parseInt(input[1]);
-            
-            if (w == 0 && h == 0) break;
-            
-            isVisited = new boolean[h][w];
+            // 초기화
+            visited = new boolean[h][w];
             arr = new int[h][w];
-            
-            //배열 채우기 
-            for(int i=0; i<h; i++) {
-            	StringTokenizer st = new StringTokenizer(br.readLine());
-            	for(int j=0; j<w; j++) {
-            		arr[i][j] = Integer.parseInt(st.nextToken());
-            	}
+
+            for(int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < w; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
             }
-            
-            int cnt=0;
-   
-            for(int i=0; i<h; i++) {
-            	for(int j=0; j<w; j++) {
-            		if(arr[i][j] == 1 && !isVisited[i][j]) {
-            			dfs(i,j);
-            			cnt++;
-            		}
-            	}
+
+            cnt = 0;
+            for(int i = 0; i < h; i++) {
+                for(int j = 0; j < w; j++) {
+                    if(!visited[i][j] && arr[i][j] == 1) {
+                        dfs(i,j);
+                        cnt++;
+                    }
+                }
             }
-            sb.append(cnt).append("\n");
-           
-           
+            System.out.println(cnt);
+
+
         }
-        
-        System.out.println(sb);
+
+
+
     }
 
-	private static void dfs(int x, int y) {
-		isVisited[x][y] = true;
-		
-		for(int i=0; i<8; i++) {
-			int nx = x+dx[i];
-			int ny = y+dy[i];
-			
-			if(nx>=0 && ny>=0 && nx<h && ny<w) {
-				if(arr[nx][ny]==1 && !isVisited[nx][ny]) {
-					dfs(nx,ny);
-				}
-			}
-		}
-		
-	}
-	
-		
-	
+    static void dfs (int x, int y) {
+        visited[x][y] = true;
+
+        for(int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx>=0 && ny>=0 && nx<h && ny<w) {
+                if(!visited[nx][ny] && arr[nx][ny] == 1) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+
 }
 
