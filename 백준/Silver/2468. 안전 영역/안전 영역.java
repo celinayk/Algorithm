1bb7c36d6940b2456bc4e0c8c59372ef9bbbf81f
YeonKyung Ryu <80758099+celinayk@users.noreply.github.com>
2026-03-23 11:11:38 +0900

    [Silver I] Title: 안전 영역, Time: 240 ms, Memory: 20372 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,81 +1,71 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+
 
 public class Main {
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	static int n;
-	static int max = 0;
-    static int cnt = 0;
-	static int[][] arr;
-	static boolean[][] visited;
+    static int n;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        
+
         arr = new int[n][n];
-        visited = new boolean[n][n];
+
+        int max = 0;
         StringTokenizer st;
-        int maxheight=0;
-        for(int i=0; i<n; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	for(int j=0; j<n; j++) {
-        		arr[i][j] = Integer.parseInt(st.nextToken());
-        		maxheight=Math.max(maxheight, arr[i][j]);
-        		
-        	}
-        }
-        
-      
-        //높이 0부터 최대높이까지 
-        for(int k=0; k<=maxheight; k++) {
-        	cnt=0;
-        	for(int i=0; i<n; i++) {
-        		for(int j=0; j<n; j++) {
-        			if(arr[i][j]<=k) { //높이보다 작은 칸은 침수 
-        				visited[i][j]= false;
-        			}
-        			else {
-        				visited[i][j]=true;
-        			}
-        		}
-        	}
-        	
-        	for(int i=0; i<n; i++) {
-            	for(int j=0; j<n; j++) {
-            		if(visited[i][j]==true) {
-            			cnt++;
-            			dfs(i,j);
-            		}
-            	}
+        // 입력받기
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                max=Math.max(arr[i][j], max);
             }
-            max=Math.max(cnt, max);
         }
-        System.out.println(max);
-        
-        
+
+        int ans = 1;
+        for(int h=1; h<=max; h++) { // 높이는 배열의 최댓값까지만 보면됨
+            visited = new boolean[n][n]; // 루프 돌때마다 방문배열 초기화
+            int cnt = 0;
+
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(arr[i][j] <= h) {
+                        visited[i][j] = true; // 잠김 처리
+                    }
+                }
+            }
+
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(!visited[i][j]) {
+                        dfs(i,j);
+                        cnt++;
+                    }
+                }
+            }
+            ans = Math.max(cnt, ans);
+
+        }
+        System.out.println(ans);
     }
-	private static void dfs(int x, int y) {
-		visited[x][y] = false;
-		
-		for(int i=0; i<4; i++) {
-			int nx = dx[i]+x;
-			int ny = dy[i]+y;
-			
-			if(nx>=0 && ny>=0 && nx<n && ny<n) {
-				if(visited[nx][ny]==true) {
-					dfs(nx,ny);
-				}
-			}
-		}
-		
-	}
 
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
 
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx>=0 && ny>=0 && nx<n && ny<n) {
+                if(!visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
 }
 
